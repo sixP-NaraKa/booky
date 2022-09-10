0c4def02af3874bbcb1ec40ef8385854271c382d
sixP-NaraKa <71403526+sixP-NaraKa@users.noreply.github.com>
2022-09-10 15:08:10 +0200

    Fix error upon trying to open QR code output directory with empty value from saved property.

--- src/main/java/gui/util/Chooser.java
+++ src/main/java/gui/util/Chooser.java
@@ -10,13 +10,17 @@ public final class Chooser {
 
     public static File ofDirectory(File initialDirectory) {
         final DirectoryChooser directoryChooser = new DirectoryChooser();
-        directoryChooser.setInitialDirectory(initialDirectory);
+        if (initialDirectory != null && initialDirectory.exists() && initialDirectory.isDirectory()) {
+            directoryChooser.setInitialDirectory(initialDirectory);
+        }
         return directoryChooser.showDialog(new Stage());
     }
 
     public static File ofFileOpen(File initialDirectory) {
         final FileChooser fileChooser = new FileChooser();
-        fileChooser.setInitialDirectory(initialDirectory);
+        if (initialDirectory != null && initialDirectory.exists() && initialDirectory.isDirectory()) {
+            fileChooser.setInitialDirectory(initialDirectory);
+        }
         final FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("QR Codes",
                 "*.png", "*.jpg", "*.jpeg");
         fileChooser.getExtensionFilters().add(extensionFilter);
